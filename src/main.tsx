aa47129bd80bc81e6a66d88e0cbcad6e38a99aae
Qasim Ali <qasim.aliii019@gmail.com>
2026-09-14 14:40:37 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 import { store } from "./components/redux/store";
@@ -10,7 +12,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <TodoContextProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </TodoContextProvider>
     </Provider>
   </StrictMode>
